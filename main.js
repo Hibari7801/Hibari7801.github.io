cdd4ac68ff0078695e95a24d5b74ac73366b09b9
Alondra <137462807+Hibari7801@users.noreply.github.com>
2024-12-08 21:21:48 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ var typewriter = new Typewriter(app, {
     loop: true
 });
 
-typewriter.typeString('Desarrolladora?') // texto que aparece primero
+typewriter.typeString('Actuaria') // texto que aparece primero
     .pauseFor(2500) //cuanto esperará
     .deleteAll()
-    .typeString('We will meet again on the winter')
+    .typeString('El fracaso no es un desastre')
     .pauseFor(2500)
-    .deleteChars(13) //numero de caracteres que se borran
-    .typeString('<strong> when the flowers blooms!</strong>') //texto principal
+    .deleteChars(17) //numero de caracteres que se borran
+    .typeString('<strong>es éxito si aprendemos de él.</strong>') //texto principal
     .pauseFor(2500)
     .start();
